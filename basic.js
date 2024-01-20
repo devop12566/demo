66a0666f1c9b34b3cf1e9dc5efe4da59f6fcf545
Dev Trehan <111063257+devop12566@users.noreply.github.com>
2024-01-20 13:38:57 +0000

    add

--- basic.js
+++ basic.js
@@ -1,1 +1,10 @@
-console.log("dev") 
+const accountId = 123
+let emailId = "dev@open"
+var password = "123456789"
+city = "mohali"
+
+emailId ="tctc@open"
+password="3009"
+city="chandigarh"
+console.table([emailId, password, city ])
+
